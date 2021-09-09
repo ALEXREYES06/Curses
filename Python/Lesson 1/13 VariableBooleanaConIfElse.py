4ce19532042febffc05fd94fa2a0d6e145477b57
ALEXREYES06 <87919180+ALEXREYES06@users.noreply.github.com>
2021-09-09 17:24:37 -0500

    added new exercises

---
+++ Python/Lesson 1/13 VariableBooleanaConIfElse.py
@@ -0,0 +1,11 @@
+# Tipos bool (boolean)
+miVariable = True
+print(miVariable)
+
+miVariable = 3 > 100
+print(miVariable)
+
+if miVariable:
+    print("El resultado fue verdadero")
+else:
+    print("El resultado fue falso")
